31f133ecaa06c6d0db60418da6bff2e6a2bd5cd0
Tobias Gärdebro <tubbeg@gmail.com>
2025-01-13 19:05:30 +0100

    Added h-raylib library created functions for displaying a simple window. Planning on using apecs Entity Component System later.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,30 @@
 module Main where
 
+
+import Raylib.Core (clearBackground, beginDrawing, endDrawing, initWindow, setTargetFPS, windowShouldClose, closeWindow)
+import Raylib.Core.Text (drawText)
+import Raylib.Util (drawing, raylibApplication, WindowResources)
+import Raylib.Util.Colors (lightGray, rayWhite)
+
+
+createWindow :: () -> IO WindowResources
+createWindow () = do
+  window <- initWindow 600 450 "git gud"
+  setTargetFPS 60
+  return window
+
+drawLoop :: Bool -> WindowResources -> IO ()
+drawLoop False w = do
+    beginDrawing
+    endDrawing
+    s <- windowShouldClose
+    drawLoop s w
+drawLoop True w = do  
+    closeWindow $ Just w
+    return ()
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "hello hello hello"
+  w <- createWindow ()
+  drawLoop False w
