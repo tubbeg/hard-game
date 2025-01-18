785effc22fdd8c2c658636966745bcae5586064b
Tobias Gärdebro <tubbeg@gmail.com>
2025-01-18 18:13:34 +0100

    I think that I might replace raylib with Brick. Right now I want to make a simple game. And for that purpose I don't need raylib. A TUI (Terminal User Interface) application should be sufficient.

---
+++ app/Deck.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeFamilies               #-}
+module Deck (makeRank, Rank, Suit(Clubs, Hearts, Spades, Diamonds), rankToDefaultChips, Chips) where
+import Apecs
+
+newtype Rank = R Int
+instance Component Rank where
+    type Storage Rank = Map Rank
+
+data Suit = Clubs | Diamonds | Hearts | Spades
+instance Component Suit where
+    type Storage Suit = Map Suit
+
+newtype Chips = C Int
+instance Component Chips where
+    type Storage Chips = Map Chips
+instance Semigroup Chips where
+    (<>) (C a) (C b) = C (a + b) --associtivty
+    -- the above actually makes sense
+    -- we just need to clarify how to values of the type
+    -- are to be combined
+-- Monoid requries the semigroup class to be instanced
+instance Monoid Chips where
+    mempty = C 0 --we can use the monoid for the inital value
+    -- but this might not be necessary
+
+-- smart constructor. Do not export constructor for Rank
+makeRank :: Int -> Maybe Rank
+makeRank i
+    | i < 15 && i > 1 = Just $ R i
+    | otherwise = Nothing
+
+
+rankToDefaultChips :: Rank -> Chips
+rankToDefaultChips (R r)
+  | r > 1 && r < 11 = C r
+  | r == 15 = C 11
+  | otherwise = C 10
--- app/Main.hs
+++ app/Main.hs
@@ -11,10 +11,6 @@
 module Main where
 
 
-import Raylib.Core (clearBackground, beginDrawing, endDrawing, initWindow, setTargetFPS, windowShouldClose, closeWindow)
-import Raylib.Core.Text (drawText)
-import Raylib.Util (drawing, raylibApplication, WindowResources)
-import Raylib.Util.Colors (lightGray, rayWhite)
 import Apecs
 import Linear
 import System.Random
@@ -22,39 +18,118 @@ import System.Exit
 import Control.Monad
 import Data.Monoid
 import Data.Semigroup (Semigroup)
+import Deck(makeRank, Rank, Suit(Diamonds,Clubs,Hearts,Spades), rankToDefaultChips, Chips)
+import Data.Time.Clock
+import Data.Fixed (Pico)
+import Brick
+import qualified Graphics.Vty as V
+import qualified Brick.Widgets.Border as V.Vty
+
+{-
+
+not exporting the constructor, just the type itself
+which means the only way to create a rank is with the
+makeRank function. This means that we have guarantees.
+I'm really starting to understand why Haskell is awesome
+
+r :: Maybe Rank
+r = makeRank 15
+
+r2 :: Rank -- this will fail
+r2 = R 100
+-}
+
+newtype Order = Pos Int deriving Show
+instance Component Order where type Storage Order = Map Order
 
 
-
-newtype Position = Position (V2 Float) deriving Show
-instance Component Position where type Storage Position = Map Position
-makeWorld "World" [''Position]
+makeWorld "World" [''Order, ''Rank, ''Suit, ''Chips]
 
 type System' a = System World a
 
+
+defaultRanks :: [Rank]
+defaultRanks =
+  foldr f [] rankMaybs
+  where
+    numbers = [2..14]
+    rankMaybs = [makeRank x | x <- numbers] -- list comprehension
+    f ::  Maybe Rank -> [Rank] -> [Rank]
+    f (Just someRank) l = someRank:l
+    f Nothing rankList = rankList
+
+defaultDeck :: [(Rank, Suit)]
+defaultDeck =
+    spades ++ hearts ++ clubs ++ diamonds
+    where
+      spades = [(r, Spades) | r <- defaultRanks]
+      hearts = [(r, Hearts) | r <- defaultRanks]
+      clubs = [(r, Clubs) | r <- defaultRanks]
+      diamonds = [(r, Diamonds) | r <- defaultRanks]
+
+createDeck :: [(Rank, Suit)] -> System' ()
+createDeck [] = return ()
+createDeck ((r,s):remainingDeck) = do
+  _ <- newEntity (s, r, chips)
+  createDeck remainingDeck
+  where
+    chips = rankToDefaultChips r
+
 initialize :: System' ()
 initialize = do
+  createDeck defaultDeck
   return ()
 
-createWindow :: () -> IO WindowResources
-createWindow () = do
-  window <- initWindow 600 450 "git gud"
-  setTargetFPS 60
-  return window
+getDeltaTime :: UTCTime -> UTCTime -> Pico
+getDeltaTime a b =
+  dt
+  where
+    p = diffUTCTime a b
+    dt = nominalDiffTimeToSeconds p
 
-drawLoop :: Bool -> WindowResources -> IO ()
-drawLoop False w = do
-    beginDrawing
-    drawText "Wow so cool" 30 40 18 lightGray
-    clearBackground rayWhite
-    endDrawing
-    s <- windowShouldClose
-    drawLoop s w
-drawLoop True w = do  
-    closeWindow $ Just w
-    return ()
+
+
+ui :: Widget ()
+ui = str "Hello, world!"
+
+myApp :: App String Int Int -> String
+myApp a = ""
+
+data TickUpdate = Tick
+data GameState = MyState -- I can switch this with the ECS
+
+-- this function could update the game loop (ECS)
+drawGame :: GameState -> [Widget ()]
+drawGame gs =
+  [w]
+  where
+    w :: Widget ()
+    w = str "Hello," <=> str "World!" <=> V.Vty.hBorder
+
+
+eventHandle :: BrickEvent () TickUpdate -> EventM () GameState ()
+eventHandle be = do
+  return ()
+
+
+-- attribute maps
+-- reminds me a little bit of CSS
+dummyMap :: AttrMap
+dummyMap = attrMap V.defAttr []
 
 main :: IO ()
 main = do
-  putStrLn "hello hello hello"
-  w <- createWindow ()
-  drawLoop False w
+  _ <- defaultMain app initState
+  return ()
+  where
+    initState = MyState
+    -- using Unit () as name. Name would have to derive/implement Ord class
+    app :: App GameState TickUpdate ()
+    app = App {
+            appDraw         = drawGame
+          , appChooseCursor = neverShowCursor
+          , appHandleEvent  = eventHandle
+          , appStartEvent   = return () -- do nothing
+          , appAttrMap      = const $ dummyMap
+
+          }
